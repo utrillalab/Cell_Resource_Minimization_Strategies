f2d89db71005c74ca898bd6261c441ef30003a8a
Elisa Márquez Zavala <emarquez@lcg.unam.mx>
2022-05-18 20:01:18 +0200

    ME UPF energy analysis

--- src/energy_analisis.py
+++ src/energy_analisis.py
@@ -8,10 +8,10 @@ def get_energy_consumption_production(me):
     muopt = me.solution.x[0]
     mu=muopt
     reactions_no_energy_production=[]
-    reactions_energy_production={'Reactions':[],'Type':[],'Total':[],'Used':[],'ATP':[]}
+    reactions_energy_production={'Reactions':[],'Type':[],'ATP':[],'Used':[]}
 
     reactions_no_energy_production_consumption=[]
-    reactions_energy_consumption={'Reactions':[],'Type':[],'Total':[],'Used':[],'ATP':[]}
+    reactions_energy_consumption={'Reactions':[],'Type':[],'ATP':[],'Used':[]}
     for reaction in  me.reactions:
 
         if atp_c not in reaction.metabolites and gtp_c not in reaction.metabolites:
@@ -52,13 +52,11 @@ def get_energy_consumption_production(me):
             if (produce_atp or produce_gtp) and reaction.x > 0:
                 reactions_energy_production['Reactions'].append((reaction))
                 reactions_energy_production['Type'].append((type(reaction).__name__))
-                reactions_energy_production['Total'].append((atp_value))
                 reactions_energy_production['Used'].append((reaction.x*(atp_value)))
                 reactions_energy_production['ATP'].append((atp_value))
             elif (consume_atp) and reaction.x > 0:
                 reactions_energy_consumption['Reactions'].append(str(reaction))
                 reactions_energy_consumption['Type'].append((type(reaction).__name__))
-                reactions_energy_consumption['Total'].append((atp_value))
                 reactions_energy_consumption['Used'].append((reaction.x*(atp_value)))
                 reactions_energy_consumption['ATP'].append((atp_value))
     return(reactions_energy_consumption,reactions_energy_production)
